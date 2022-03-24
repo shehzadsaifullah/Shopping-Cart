cf010c98fe7350a021d48e095f2142156720712f
shehzadsaifullah <shehzadsaifullah@gmail.com>
2022-03-24 17:59:37 +0500

    whatttttt

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <h1>HELLO</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
