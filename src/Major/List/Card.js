b8aaf212622114ce426957b40e48be228b8167fc
shehzadsaifullah <shehzadsaifullah@gmail.com>
2022-03-26 20:18:54 +0500

    useContext

--- src/Major/List/Card.js
+++ src/Major/List/Card.js
@@ -1,4 +1,10 @@
+import { useRef } from "react";
+
 const Card = (props) => {
+  const inputQuantityValue = useRef();
+  const addToCartHandler = () => {
+    console.log(props.listitem.id, inputQuantityValue.current.value);
+  };
   return (
     <div>
       <div>
@@ -8,9 +14,10 @@ const Card = (props) => {
           <div>{props.listitem.price}</div>
         </ul>
       </div>
-      <div>quantity</div>
       <div>
-        <button>add to card</button>
+        <label>Quantity</label>
+        <input type="number" min="1" max="5" ref={inputQuantityValue}></input>
+        <button onClick={addToCartHandler}>add to card</button>
       </div>
     </div>
   );
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,6 @@
 import CartContext from "./CartContext";
 import { useReducer } from "react";
+
 const CartProvider = (props) => {
   const addCardItemHandler = (item) => {};
   const removeCartItemHandler = (id) => {};
